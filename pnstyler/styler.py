f1874801d559378fae986e95337e30537ea13746
Pietro Novelli <pietronvll@gmail.com>
2024-02-21 07:12:25 +0100

    Edited default color palette

--- pnstyler/styler.py
+++ pnstyler/styler.py
@@ -90,10 +90,32 @@ palettes = {
         "#022b30",
         "#081a1c",
     ],
+    "pastel": [
+        "#fd7f6f",
+        "#7eb0d5",
+        "#b2e061",
+        "#bd7ebe",
+        "#ffb55a",
+        "#ffee65",
+        "#beb9db",
+        "#fdcce5",
+        "#8bd3c7",
+    ],
+    "bright": [
+        "#e60049",
+        "#0bb4ff",
+        "#50e991",
+        "#e6d800",
+        "#9b19f5",
+        "#ffa300",
+        "#dc0ab4",
+        "#b3d4ff",
+        "#00bfa0",
+    ],
 }
 
 
-def set_color_palette(palette_name="categorical_light"):
+def set_color_palette(palette_name="pastel"):
     plt.rcParams["axes.prop_cycle"] = cycler(color=palettes[palette_name])
 
 
